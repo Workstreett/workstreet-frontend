661f601d30b1998e7f259a71a88c1176f98058d8
Bharat Bansal <bharat.bansal.cd.eee19@itbhu.ac.in>
2021-09-15 14:55:36 +0530

    poster phone view corrected

--- src/components/pages/Companies.js
+++ src/components/pages/Companies.js
@@ -1,42 +1,12 @@
 import React from 'react'
 import '../../css/appage.css'
 import PosterGrid from '../PosterGrid.js'
-import PosterPhoneGrid from '../PosterPhoneGrid.js'
 
 class Companies extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            isDesktop: false
-        }
-
-        this.updatePredicate = this.updatePredicate.bind(this)
-    }
-
-    componentDidMount() {
-        this.updatePredicate()
-        window.addEventListener('resize', this.updatePredicate)
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.updatePredicate)
-    }
-
-    updatePredicate() {
-        this.setState({ isDesktop: window.innerWidth > 850 })
-    }
-
     render() {
-        const isDesktop = this.state.isDesktop
         return (
             <div>
-                {isDesktop ? (
-                    <div>
-                        <PosterGrid />
-                    </div>
-                ) : (
-                    <PosterPhoneGrid />
-                )}
+                <PosterGrid />
             </div>
         )
     }
