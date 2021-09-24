3973ae252c7a602d8f53ac8ea24bdee85cae60d6
Bharat Bansal <bharat.bansal.cd.eee19@itbhu.ac.in>
2021-09-24 22:40:03 +0530

    sidebar hover ui card also

--- src/components/PosterGrid.js
+++ src/components/PosterGrid.js
@@ -7,46 +7,41 @@ class PosterGrid extends React.Component {
     render() {
         return (
             <div className="poster-grid-main">
-                <div className="poster-grid">
-                    <Poster
-                        id="one"
-                        icon={img1}
-                        name="GOOGLE"
-                        compdesc="Our mission is to organise the world’s information and make it universally accessible and useful."
-                        role="Software Engineer"
-                        roledesc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
-                    />
-                </div>
-                <div className="poster-grid">
-                    <Poster
-                        id="one"
-                        icon={img1}
-                        name="GOOGLE"
-                        compdesc="Our mission is to organise the world’s information and make it universally accessible and useful."
-                        role="Software Engineer"
-                        roledesc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
-                    />
-                </div>
-                <div className="poster-grid">
-                    <Poster
-                        id="one"
-                        icon={img1}
-                        name="GOOGLE"
-                        compdesc="Our mission is to organise the world’s information and make it universally accessible and useful."
-                        role="Software Engineer"
-                        roledesc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
-                    />
-                </div>
-                <div className="poster-grid">
-                    <Poster
-                        id="one"
-                        icon={img1}
-                        name="GOOGLE"
-                        compdesc="Our mission is to organise the world’s information and make it universally accessible and useful."
-                        role="Software Engineer"
-                        roledesc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
-                    />
-                </div>
+                <Poster
+                    id="one"
+                    icon={img1}
+                    name="GOOGLE"
+                    compdesc="Our mission is to organise the world’s information and make it universally accessible and useful."
+                    role="Software Engineer"
+                    roledesc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled"
+                />
+
+                <Poster
+                    id="one"
+                    icon={img1}
+                    name="GOOGLE"
+                    compdesc="Our mission is to organise the world’s information and make it universally accessible and useful."
+                    role="Software Engineer"
+                    roledesc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled"
+                />
+
+                <Poster
+                    id="one"
+                    icon={img1}
+                    name="GOOGLE"
+                    compdesc="Our mission is to organise the world’s information and make it universally accessible and useful."
+                    role="Software Engineer"
+                    roledesc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled"
+                />
+
+                <Poster
+                    id="one"
+                    icon={img1}
+                    name="GOOGLE"
+                    compdesc="Our mission is to organise the world’s information and make it universally accessible and useful."
+                    role="Software Engineer"
+                    roledesc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled"
+                />
             </div>
         )
     }
