afb7c7487b223e149ee842f245a82fc346d02524
Your Name <you@example.com>
2021-12-19 10:55:15 +0530

    landing part 1 and 2 added

---
+++ src/components/Landing1.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Landing1 extends React.Component {
+    render() {
+        return (
+            <div>
+                <form>
+                    <input type="text" name="Enter Your Email Address" />
+                    <input type="button" value="Suscribe" />
+                </form>
+            </div>
+        )
+    }
+}
+export default Landing1
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import '../css/Landing.css'
+import Landing1 from '../components/Landing1'
+import Landing2 from '../components/Landing2'
 import Landing3 from '../components/Landing3'
 import Landing4 from '../components/Landing4'
 
@@ -7,8 +9,8 @@ class Landing extends React.Component {
     render() {
         return (
             <div className="landing-grid">
-                <div>Landing1</div>
-                <div>Landing2</div>
+                <Landing1 />
+                <Landing2 />
                 <Landing3 />
                 <Landing4 />
             </div>
