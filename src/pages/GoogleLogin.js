f9325faef04d3ffab59d28db88b1d08070bcb51d
Pradeep Sharma <pradeep.sharma.cd.mec19@itbhu.ac.in>
2022-01-06 09:56:48 +0530

    Added Responsiveness

--- src/pages/GoogleLogin.js
+++ src/pages/GoogleLogin.js
@@ -7,13 +7,17 @@ class GoogleLogin extends React.Component {
         return (
             <div className="Glogin">
                 <div className="Glogin-box">
-                    <img src={logo} alt="Workstreet-logo" />
-                    <h1>One step solution to track your application status</h1>
-                    <button>
-                        <i className="fab fa-google-plus-g Glogin-icon"></i>
-                        Sign in With Google
-                    </button>
-                    <div className="copyright"> &copy;2022 All Right Reserved</div>
+                    <div>
+                        <img src={logo} alt="Workstreet-logo" />
+                        <h1>One step solution to track your application status</h1>
+                    </div>
+                    <div>
+                        <button>
+                            <i className="fab fa-google-plus-g Glogin-icon"></i>
+                            Sign in With Google
+                        </button>
+                        <div className="copyright"> &copy;2022 All Right Reserved</div>
+                    </div>
                 </div>
             </div>
         )
