db62e24be0b9aa8543a08d2a7b698d39019e3ad2
Bharat-mtr <bharatagmtr.999@gmail.com>
2022-01-06 21:00:25 +0530

    company applied page

--- src/components/Company.js
+++ src/components/Company.js
@@ -2,7 +2,50 @@ import React from 'react'
 import '../css/Company.css'
 
 class Company extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            status: 'nothing',
+            color: ''
+        }
+    }
+
+    componentDidMount() {
+        if (this.props.status === 1) {
+            console.log('first')
+            this.setState({ status: 'Accepted', color: '#008000' })
+        } else if (this.props.status === 0) {
+            console.log('second')
+            this.setState({ status: 'Process', color: '#FFBF00' })
+        } else {
+            console.log('third')
+            this.setState({ status: 'Rejected', color: '#ff0000' })
+        }
+    }
+
     render() {
-        return <div></div>
+        return (
+            <div className="company-card">
+                <img src={this.props.logo} alt="comapny-logo" />
+                <hr />
+                <div className="comapny-info">{this.props.info}</div>
+                <div className="company-shortdetails">
+                    <div>
+                        <b>Applied Role: </b>
+                        {this.props.role}
+                    </div>
+                    <div>
+                        <b>Applied Date: </b>
+                        {this.props.date}
+                    </div>
+                    <div style={{ color: this.state.color }}>
+                        <b style={{ color: '#000000' }}>Status: </b>
+                        <b>{this.state.status}</b>
+                    </div>
+                </div>
+                <button className="company-trackbutton">Track Application</button>
+            </div>
+        )
     }
 }
+export default Company
--- src/pages/AppliedCompany.js
+++ src/pages/AppliedCompany.js
@@ -2,6 +2,7 @@ import React from 'react'
 import '../css/AppliedCompany.css'
 import Company from '../components/Company'
 import logo from '../images/logo.svg'
+import NbliK from '../images/NbliK.png'
 
 class AppliedCompany extends React.Component {
     render() {
@@ -13,22 +14,49 @@ class AppliedCompany extends React.Component {
                         <i className="fas fa-power-off fa-x"></i> Logout
                     </button>
                 </div>
-                <hr style={{ border: '1px solid #071E3D', marginBottom: '20px' }} />
-                <div>
-                    <div className="applied-user-info">
-                        <img
-                            src="https://lh3.googleusercontent.com/a/AATXAJy3xWOki6DHu_Kh1Ripe8bJQ5f6-70QcKxKMffl=s288-p-rw-no-mo"
-                            alt="user-dp"
-                        />
-                        <div>
-                            <p className="greeting">
-                                Welcome, <span style={{ color: '#FFA45C' }}>Jhon Doe</span>
-                            </p>
-                            <p className="number">Total aplication: 3</p>
-                        </div>
+                <hr style={{ height: '2px', borderWidth: '0px', backgroundColor: '#000000' }} />
+                <div className="applied-user-info">
+                    <img
+                        src="https://lh3.googleusercontent.com/a/AATXAJy3xWOki6DHu_Kh1Ripe8bJQ5f6-70QcKxKMffl=s288-p-rw-no-mo"
+                        alt="user-dp"
+                    />
+                    <div>
+                        <p className="greeting">
+                            Welcome, <span style={{ color: '#FFA45C' }}>Jhon Doe</span>
+                        </p>
+                        <p className="number">Total aplication: 3</p>
                     </div>
                 </div>
-                <h3>Your Application</h3>
+                <h3 className="line-comapany">Your Application</h3>
+                <div className="applied-list">
+                    <Company
+                        logo={NbliK}
+                        info="Made in India, NbliK is an interest-based community and a content-sharing platform where you can explore top-notch content and make friends who share your interests.Browse articles & topics as per your interests.
+
+                        Discover topics like: time management, productivity, health, money, communication, teamwork, personal growth, leadership, and several others!"
+                        role="Business Development"
+                        date="21/10/2021"
+                        status={1}
+                    />
+                    <Company
+                        logo={NbliK}
+                        info="Made in India, NbliK is an interest-based community and a content-sharing platform where you can explore top-notch content and make friends who share your interests.Browse articles & topics as per your interests.
+
+                        Discover topics like: time management, productivity, health, money, communication, teamwork, personal growth, leadership, and several others!"
+                        role="Business Development"
+                        date="21/10/2021"
+                        status={0}
+                    />
+                    <Company
+                        logo={NbliK}
+                        info="Made in India, NbliK is an interest-based community and a content-sharing platform where you can explore top-notch content and make friends who share your interests.Browse articles & topics as per your interests.
+
+                        Discover topics like: time management, productivity, health, money, communication, teamwork, personal growth, leadership, and several others!"
+                        role="Business Development"
+                        date="21/10/2021"
+                        status={-1}
+                    />
+                </div>
             </div>
         )
     }
