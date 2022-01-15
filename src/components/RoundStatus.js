e2f2df86c569274f70f6c71e672b2af7228ff7e2
Bharat-mtr <bharatagmtr.999@gmail.com>
2022-01-16 00:40:22 +0530

    trackstatus ui

--- src/components/RoundStatus.js
+++ src/components/RoundStatus.js
@@ -3,70 +3,64 @@ import '../css/RoundStatus.css'
 
 export const Rounds = [
     {
-        icon: 'fas fa-circle',
+        icon: 'fas fa-file',
         name: 'Resume Round',
         date: '12/10/2021',
-        review: 'Good Resume, you may develop on your python, js and React Skills. Forwarded for next Round blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah '
+        review: 'Good Resume, you may develop on your python, js and React Skills. Forwarded for next Round blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ',
+        status: true
     },
     {
-        icon: 'fas fa-circle-notch',
-        name: 'Resume Round',
+        icon: 'far fa-check-circle',
+        name: 'Task Round',
         date: '12/10/2021',
-        review: 'Good Resume, you may develop on your python, js and React Skills. Forwarded for next Round'
+        review: 'Good Resume, you may develop on your python, js and React Skills. Forwarded for next Round',
+        status: true
     },
     {
-        icon: 'fas fa-circle-notch',
-        name: 'Resume Round',
+        icon: 'far fa-check-circle',
+        name: 'Task Round-2',
         date: '12/10/2021',
-        review: 'Good Resume, you may develop on your python, js and React Skills. Forwarded for next Round'
+        review: 'Good Resume, you may develop on your python, js and React Skills. Forwarded for next Round',
+        status: true
     },
     {
-        icon: 'fas fa-circle-notch',
-        name: 'Resume Round',
+        icon: 'far fa-check-circle',
+        name: 'Task-3 round',
         date: '12/10/2021',
-        review: 'Good Resume, you may develop on your python, js and React Skills. Forwarded for next Round'
+        review: 'Good Resume, you may develop on your python, js and React Skills. Forwarded for next Round',
+        status: false
     },
     {
-        icon: 'fas fa-circle-notch',
-        name: 'Resume Round',
+        icon: 'fas fa-user-friends',
+        name: 'Interview Round',
         date: '12/10/2021',
-        review: 'Good Resume, you may develop on your python, js and React Skills. Forwarded for next Round'
+        review: 'Good Resume, you may develop on your python, js and React Skills. Forwarded for next Round',
+        status: false
     },
     {
-        icon: 'fas fa-circle-notch',
-        name: 'Resume Round',
+        icon: 'fas fa-user-friends',
+        name: 'Interview-2 Round',
         date: '12/10/2021',
-        review: 'Good Resume, you may develop on your python, js and React Skills. Forwarded for next Round'
-    },
-    {
-        icon: 'fas fa-circle-notch',
-        name: 'Resume Round',
-        date: '12/10/2021',
-        review: 'Good Resume, you may develop on your python, js and React Skills. Forwarded for next Round'
-    },
-    {
-        icon: 'fas fa-circle-notch',
-        name: 'Resume Round',
-        date: '12/10/2021',
-        review: 'Good Resume, you may develop on your python, js and React Skills. Forwarded for next Round'
+        review: 'Good Resume, you may develop on your python, js and React Skills. Forwarded for next Round',
+        status: false
     }
 ]
 
-class RoundStatus extends React.Component {
-    render() {
-        return (
-            <div className="roundstatus-flex">
-                <div style={{ padding: '2%', backgroundColor: 'green', borderRadius: '50%' }}>
-                    <i className="fas fa-circle-notch"></i>
-                </div>
-                <div className="roundstatus-text">
-                    {this.props.name}
-                    <div style={{ fontSize: '12px', color: '#6e6d76' }}>{this.props.date}</div>
-                </div>
-                <div className="roundstatus-review">&quot;{this.props.review}&quot;</div>
-            </div>
-        )
-    }
-}
+// class RoundStatus extends React.Component {
+//     render() {
+//         return (
+//             <div className="roundstatus-flex">
+//                 <div style={{ padding: '2%', backgroundColor: 'green', borderRadius: '50%' }}>
+//                     <i className="fas fa-circle-notch"></i>
+//                 </div>
+//                 <div className="roundstatus-text">
+//                     {this.props.name}
+//                     <div style={{ fontSize: '12px', color: '#6e6d76' }}>{this.props.date}</div>
+//                 </div>
+//                 <div className="roundstatus-review">&quot;{this.props.review}&quot;</div>
+//             </div>
+//         )
+//     }
+// }
 
-export default RoundStatus
+// export default RoundStatus
--- src/pages/CompanyStatus.js
+++ src/pages/CompanyStatus.js
@@ -12,58 +12,66 @@ class CompanyStatus extends React.Component {
             <div>
                 <UserHeader />
                 <div className="compStatus-flex">
-                    <div className="compstatus-c1">
-                        <div className="compstatus-user">
-                            <img
-                                src="https://lh3.googleusercontent.com/a/AATXAJy3xWOki6DHu_Kh1Ripe8bJQ5f6-70QcKxKMffl=s288-p-rw-no-mo"
-                                alt="user-dp"
-                            />
+                    <div className="your-status-line">
+                        <div className="your-status-heading">
+                            <h3>Track Your Status </h3>
+                            <h3>For NbilK</h3>
+                        </div>
+                        <div className="your-status-message">
                             <p>
-                                Welcome, <span style={{ color: '#FFA45C' }}>Jhon Doe</span>
+                                Till now you have successfuly completed resume round and interview
+                                round. Wait for final round.
+                            </p>
+                            <p>
+                                {' '}
+                                We have got a tracker for you just check our current status and
+                                track it as well. We make sure to make you feel conacted to us and
+                                company.
                             </p>
                         </div>
-                        <Company
-                            logo={NbliK}
-                            info="Made in India, NbliK is an interest-based community and a content-sharing platform where you can explore top-notch content and make friends who share your interests.Browse articles & topics as per your interests.
-                        Discover topics like: time management, productivity, health, money, communication, teamwork, personal growth, leadership, and several others!"
-                            role="Business Development"
-                            date="21/10/2021"
-                            status={1}
-                            track={false}
-                        />
                     </div>
-                    <div className="compstatus-c2">
-                        <div className="compstatus-tracktext">Track Status</div>
-                        <hr
-                            style={{
-                                height: '1px',
-                                borderWidth: '0px',
-                                backgroundColor: '#000000'
-                            }}
-                        />
-                        <div className="roundtstatus-portion">
-                            {Rounds.map((round, index) => (
-                                <>
-                                    <div
-                                        className="icon"
-                                        style={{
-                                            position: 'relative'
-                                        }}
-                                    >
-                                        <i className={round.icon}></i>
+                    <Company
+                        logo={NbliK}
+                        info="Made in India, NbliK is an interest-based community and a content-sharing platform where you can explore top-notch content and make friends who share your interests.Browse articles & topics as per your interests.
+                        Discover topics like: time management, productivity, health, money, communication, teamwork, personal growth, leadership, and several others!"
+                        role="Business Development"
+                        date="21/10/2021"
+                        status={1}
+                        track={false}
+                    />
+                </div>
+                <div className="compstatus-c2">
+                    <div className="compstatus-tracktext">Track Status</div>
+                    <hr
+                        style={{
+                            height: '1px',
+                            borderWidth: '0px',
+                            backgroundColor: '#000000'
+                        }}
+                    />
+                    <div className="roundtstatus-portion">
+                        {Rounds.map((round, index) => (
+                            <>
+                                <div
+                                    className="icon"
+                                    style={{
+                                        position: 'relative'
+                                    }}
+                                >
+                                    <i
+                                        className={round.icon}
+                                        style={{ color: round.status ? 'green' : '#bababa' }}
+                                    ></i>
+                                </div>
+                                <div className="roundstatus-text">
+                                    {round.name}
+                                    <div style={{ fontSize: '12px', color: '#6e6d76' }}>
+                                        {round.date}
                                     </div>
-                                    <div className="roundstatus-text">
-                                        {round.name}
-                                        <div style={{ fontSize: '12px', color: '#6e6d76' }}>
-                                            {round.date}
-                                        </div>
-                                    </div>
-                                    <div className="roundstatus-review">
-                                        &quot;{round.review}&quot;
-                                    </div>
-                                </>
-                            ))}
-                        </div>
+                                </div>
+                                <div className="roundstatus-review">&quot;{round.review}&quot;</div>
+                            </>
+                        ))}
                     </div>
                 </div>
                 <Landing4 />
